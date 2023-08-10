5fbc10e99cd87f9ed33859df81df9d7cf1f0eb1f
Vajshi, Jayesh <jayesh.bhatu@publicissapient.com>
2023-08-10 18:21:37 +0530

    jenkins changes

--- module2/src/main/java/com/kn/poc/module2/Main2.java
+++ module2/src/main/java/com/kn/poc/module2/Main2.java
@@ -12,7 +12,7 @@ public class Main2 {
   public static void main(String[] args) {
 
     Main1.main(args);
-    System.out.println("Hello from Module 2123523");
+    System.out.println("Hello from Module 212352");
   }
 
 }
